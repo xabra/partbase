4055741d8d390c9d1f9b8878c092e2b474c84df8
xabra <adam.brailove@gmail.com>
2015-04-12 22:24:38 -0700

    Improvements in client side CRUD on accounts collection

--- public/app/accounts/accounts-module.js
+++ public/app/accounts/accounts-module.js
@@ -4,23 +4,54 @@ angular.module('accountsModule', [])
 // --- Accounts list controller ---
 .controller('accountsListCtrl', ['$scope', 'accountsService', '$location',
    function($scope, accountsService, $location) {
-      var self = this;     // Capture scope
+      var self = this; // Capture scope
 
       // Init
       self.accounts = [];
       self.count = null;
 
-      accountsService.count().success(function(response){
+      accountsService.count().success(function(response) {
          self.count = response.count;
       });
 
       accountsService.list().
-      success(function(response){
+      success(function(response) {
          self.accounts = response;
       }).
       error(function(response, status) { // Otherwise, error during GET
          console.log('ERR: accountsListCtrl: list(): Status: ' + status);
       });
+
+
+      self.setSelectionStatus = function(status) {
+         console.log("DISABLE SELECTION - TBI"+ "Set Status=" + status);
+      }
+
+      self.deleteSelection = function() {
+         console.log("DELETE SELECTION - TBI");
+      }
+
+      self.setItemStatus = function(id, status) {
+         console.log("ITEM ID = " + id + "Set Status=" + status);
+      }
+
+      self.deleteItem = function(id) {
+         console.log("DELETE !! ITEM ID = " + id);
+         accountsService.delete(id).
+         success(function(response) {
+            console.log("DELETE/SUCCESS. Server Response: " + JSON.stringify(response));
+            if (response.success) { // If the delete operation was successful...
+               //  Need to re populate the accounts array by calling list....
+            };
+         }).
+         error(function(response, status) {
+            alert('Delete error!');
+         })
+      }
+
+      self.editItem = function(id) {
+         console.log("EDIT ITEM ID = " + id);
+      }
    }
 ])
 
@@ -60,7 +91,7 @@ angular.module('accountsModule', [])
 
    //----- GET Number of items in the collection
    service.count = function() {
-      return $http.get('/api/accounts/count');     // HTTP Get data. Return a promise
+      return $http.get('/api/accounts/count'); // HTTP Get data. Return a promise
    }
 
    //----- GET all items
@@ -68,15 +99,17 @@ angular.module('accountsModule', [])
       return $http.get('/api/accounts');
    }
 
-   //----- DELETE an entry by _id
-   service.delete = function(id) {
-      return $http.delete('/api/accounts/' + id );
-   }
-
    //----- GET one item by id
    service.getById = function(id) {
       return $http.get('/api/accounts/:id');
    }
+
+   //----- DELETE an entry by _id
+   service.delete = function(id) {
+      return $http.delete('/api/accounts/' + id);
+   }
+
+
 
    return service;
 });
